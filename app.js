ddc217cee64c84012d32949cd2467ffde1618ace
JefferySCZ <jeffery.sim.cz@gmail.com>
2022-09-27 16:22:45 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,7 +21,7 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('123')
+  res.render('index')
 })
 
 app.listen(3000, () => {
